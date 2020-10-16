2e3d5131af1695c7c065553a7fabb9b60da341b4
maokaiwu <maokaiwu@aijiatui.com>
2020-10-16 14:15:48 +0800

    引入Druid连接池

--- src/main/java/com/mk/demoonspringboot/DemoOnSpringbootApplication.java
+++ src/main/java/com/mk/demoonspringboot/DemoOnSpringbootApplication.java
@@ -1,7 +1,9 @@
 package com.mk.demoonspringboot;
 
+import com.mk.demoonspringboot.mybatis_plus.mapper.UserMapper;
 import io.swagger.annotations.ApiOperation;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,5 +22,14 @@ public class DemoOnSpringbootApplication {
 	@GetMapping("/test")
 	public String test() {
 		return "http ok";
+	}
+
+	@Autowired
+	private UserMapper userMapper;
+
+	@ApiOperation("测试数据库")
+	@GetMapping("/test-db")
+	public Object testDb() {
+		return userMapper.selectById(1L);
 	}
 }
